2dd7b01dd9a0d79fb5f5ce60fbe46d7bafeab5bc
Heidi Smith <heidi.smith@ramseysolutions.com>
2025-10-02 07:49:33 -0500

    Add array of songs to Tracks component

--- src/Components/Track/Track.jsx
+++ src/Components/Track/Track.jsx
@@ -1,17 +1,41 @@
 import './Track.css';
 
 function Track() {
-    const songInfo = {
+    const songs = [
+        {
         name: "Hearing Double",
         artist: "Jason Mraz",
         album: "Look For The Good",
-        id: "",
-    };
+        id: "1",
+    },
+        {
+        name: "Unwritten",
+        artist: "Natasha Bedingfield",
+        album: "Unwritten",
+        id: "2",
+    },
+        {
+        name: "Be Kind to Yourself",
+        artist: "Andrew Peterson",
+        album: "The Burning Edge of Dawn",
+        id: "3",
+    },
+        {
+        name: "What If",
+        artist: "Cody Fry",
+        album: "The End",
+        id: "4",
+    }
+];
     return (
         <div>
-            <p>{songInfo.name}</p>
-            <p>{songInfo.artist}</p>
-            <p>{songInfo.album}</p>
+            {songs.map((song) => (
+            <p key={song.id}><strong>{song.name}</strong>
+            <br />
+            <em>{song.artist}</em>
+            <br />
+            {song.album}</p>
+            ))} 
         </div>
     );
 }
