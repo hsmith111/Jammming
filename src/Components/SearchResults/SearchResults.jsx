7adae325bbbdda612bd6eab6757362793030f0e3
Heidi Smith <heidi.smith@ramseysolutions.com>
2025-10-07 07:10:13 -0500

    Move array of songs to search results

--- src/Components/SearchResults/SearchResults.jsx
+++ src/Components/SearchResults/SearchResults.jsx
@@ -1,11 +1,51 @@
 import './SearchResults.css';
 import Track from '../Track/Track';
+import React, { useState } from 'react';
 
 function SearchResults() {
+    const [songs, setSongs] = useState([
+            {
+            name: "Hearing Double",
+            artist: "Jason Mraz",
+            album: "Look For The Good",
+            id: "1",
+        },
+            {
+            name: "Unwritten",
+            artist: "Natasha Bedingfield",
+            album: "Unwritten",
+            id: "2",
+        },
+            {
+            name: "Be Kind to Yourself",
+            artist: "Andrew Peterson",
+            album: "The Burning Edge of Dawn",
+            id: "3",
+        },
+        {
+            name: "What If",
+            artist: "Cody Fry",
+            album: "The End",
+            id: "4",
+        },
+            {
+            name: "You Belong With Me",
+            artist: "Taylor Swift",
+            album: "Fearless",
+            id: "5",
+        },
+            {
+            name: "Waiting on the World to Change",
+            artist: "John Mayer",
+            album: "Continuum",
+            id: "6",
+        }
+    ]);
+
     return (
         <div className="box searchResults">
             <h2>Search Results</h2>
-            <Track />
+            <Track songs={songs} />
         </div>
     );
 }
--- src/Components/Track/Track.jsx
+++ src/Components/Track/Track.jsx
@@ -1,58 +1,25 @@
 import './Track.css';
 import React, { useState } from 'react';
 
-function Track() {
-    const [songs, setSongs] = useState([
-        {
-        name: "Hearing Double",
-        artist: "Jason Mraz",
-        album: "Look For The Good",
-        id: "1",
-    },
-        {
-        name: "Unwritten",
-        artist: "Natasha Bedingfield",
-        album: "Unwritten",
-        id: "2",
-    },
-        {
-        name: "Be Kind to Yourself",
-        artist: "Andrew Peterson",
-        album: "The Burning Edge of Dawn",
-        id: "3",
-    },
-    {
-        name: "What If",
-        artist: "Cody Fry",
-        album: "The End",
-        id: "4",
-    },
-        {
-        name: "You Belong With Me",
-        artist: "Taylor Swift",
-        album: "Fearless",
-        id: "5",
-    },
-        {
-        name: "Waiting on the World to Change",
-        artist: "John Mayer",
-        album: "Continuum",
-        id: "6",
-    }
-]);
+function Track({ songs }) {
+
+    function handleClickToAddSong() {
+
+}
     return (
         <div>
             {songs.map((song) => (
-            <>
-            <p key={song.id}><strong>{song.name}</strong>
-            <br />
-            <em>{song.artist}</em>
-            <br />
-            {song.album}
-            </p>
-            <button>+</button>
-            <hr></hr>
-            </>
+                <div key={song.id}>
+                <p>
+                    <strong>{song.name}</strong>
+                    <br />
+                    <em>{song.artist}</em>
+                    <br />
+                    {song.album}
+                </p>
+                <button>+</button>
+                <hr />
+                </div>
             ))} 
         </div>
     );
