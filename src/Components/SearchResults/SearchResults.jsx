0ed9e9cc6aee5b5a6af2164c126363abad864435
Heidi Smith <heidi.smith@ramseysolutions.com>
2025-10-07 06:19:07 -0500

    Add list of tracks to Search Results

--- src/Components/SearchResults/SearchResults.jsx
+++ src/Components/SearchResults/SearchResults.jsx
@@ -1,9 +1,11 @@
 import './SearchResults.css';
+import Track from '../Track/Track';
 
 function SearchResults() {
     return (
         <div className="box searchResults">
             <h2>Search Results</h2>
+            <Track />
         </div>
     );
 }
--- src/Components/Tracklist/Tracklist.jsx
+++ src/Components/Tracklist/Tracklist.jsx
@@ -1,11 +1,9 @@
 import './Tracklist.css';
-import Track from '../Track/Track';
 
 function Tracklist() {
     return (
         <div className="box tracklist">
             <h2>Tracklist</h2>
-            <Track />
             <button >Save to Spotify</button>
         </div>
     );
